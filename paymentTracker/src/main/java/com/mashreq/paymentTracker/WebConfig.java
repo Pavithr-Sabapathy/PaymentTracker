d100af1e577aeb83f96f966c81f963a7d2bc3620
Pavithr-Sabapathy <pavithusaba@gmail.com>
2023-06-23 14:52:58 +0530

    Cors filter issue fixed

---
+++ paymentTracker/src/main/java/com/mashreq/paymentTracker/WebConfig.java
@@ -0,0 +1,19 @@
+package com.mashreq.paymentTracker;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		//Check the allowed origins
+		registry.addMapping("/**").allowedOrigins("/**").allowedMethods("PUT", "DELETE", "GET","POSS")
+				//.allowedHeaders("header1", "header2", "header3").exposedHeaders("header1", "header2")
+				.allowCredentials(false).maxAge(3600);
+	}
+}
