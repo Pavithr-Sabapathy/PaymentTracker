909fc39ea597a0396457285754ff90be74183128
Pavithr-Sabapathy <pavithusaba@gmail.com>
2023-07-06 16:01:29 +0530

    changes in component details query - CREATE

--- paymentTracker/src/main/resources/DB scripts/ReportComponentDetails.sql
+++ paymentTracker/src/main/resources/DB scripts/ReportComponentDetails.sql
@@ -1,9 +1,14 @@
 CREATE TABLE conf_rpt_comp_det
 (
-  id bigint NOT NULL,
-  query nvarchar(255) NOT NULL,
-  query_key nvarchar not null,
-  report_comp_id bigint NOT NULL,
-  CONSTRAINT pk_conf_rpt_com_det_id PRIMARY KEY (id ),
-  CONSTRAINT fk_report_comp_id FOREIGN KEY (report_comp_id)
-      REFERENCES conf_rpt_comp (id) ON UPDATE NO ACTION ON DELETE NO ACTION);
+   [id] [bigint] IDENTITY
+   (
+      1,
+      1
+   )
+   NOT NULL,
+   [query] [nvarchar] (2000) NOT NULL,
+   [query_key] [nvarchar] (500) NOT NULL,
+   report_comp_id bigint NOT NULL,
+   CONSTRAINT pk_conf_rpt_com_det_id PRIMARY KEY (id),
+   CONSTRAINT fk_report_comp_id FOREIGN KEY (report_comp_id) REFERENCES conf_rpt_comp (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+);
