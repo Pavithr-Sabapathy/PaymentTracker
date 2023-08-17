d99e508995ae25a95168d0fa1c5ae088245870cd
Nikita L Sutar <nikitals@fragmadata.com>
2023-08-17 15:55:21 +0530

    change in SnappReportService testcase

--- paymentTracker/src/test/java/com/mashreq/paymentTracker/serviceTest/SnappReportServicTest.java
+++ paymentTracker/src/test/java/com/mashreq/paymentTracker/serviceTest/SnappReportServicTest.java
@@ -1,14 +1,17 @@
 package com.mashreq.paymentTracker.serviceTest;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -37,7 +40,9 @@ import com.mashreq.paymentTracker.service.LinkReportService;
 import com.mashreq.paymentTracker.service.QueryExecutorService;
 import com.mashreq.paymentTracker.service.ReportConfigurationService;
 import com.mashreq.paymentTracker.service.ReportInput;
+import com.mashreq.paymentTracker.service.ReportOutput;
 import com.mashreq.paymentTracker.service.ReportOutputExecutor;
+import com.mashreq.paymentTracker.serviceImpl.SnappReportConnector;
 import com.mashreq.paymentTracker.serviceImpl.SnappReportServiceImpl;
 import com.mashreq.paymentTracker.type.CountryType;
 
@@ -63,6 +68,8 @@ public class SnappReportServicTest {
 	@MockBean
 	private LinkReportService linkReportService;
 
+	@MockBean
+	SnappReportConnector snappReportConnector;
 	@Test
 	void populateBaseInputContext() {
 
@@ -136,56 +143,17 @@ public class SnappReportServicTest {
 	}
 
 	@Test
-	void TestProcessReport() {
-		List<ReportDefaultOutput> snappReportOutputList = new ArrayList<ReportDefaultOutput>();
-		ReportDefaultOutput reportOut = new ReportDefaultOutput();
-		reportOut.setComponentDetailId(1L);
-		reportOut.setRowData(null);
-		SnappDetailedReportInput snappDetailedReportInput = new SnappDetailedReportInput();
-		FederatedReportPromptDTO referenceNumPrompt = new FederatedReportPromptDTO();
-		referenceNumPrompt.setPromptKey("ReferenceNum");
-		List<String> value = new ArrayList<String>();
-		value.add("sample");
-		referenceNumPrompt.setValueList(value);
-		snappDetailedReportInput.setReferenceNumPrompt(referenceNumPrompt);
+    public void testProcessReport() throws Exception {
+        ReportInput reportInput = new SnappDetailedReportInput();
+        ReportContext reportContext = new ReportContext();
+        ReportInstanceDTO reportInstanceDTO = new ReportInstanceDTO();
+        reportInstanceDTO.setReportName("TestReport");
+        reportContext.setReportInstance(reportInstanceDTO);
 
-		List<ReportPromptsInstanceDTO> promptsList = new ArrayList<ReportPromptsInstanceDTO>();
-		ReportPromptsInstanceDTO reportPrompt = new ReportPromptsInstanceDTO();
-		PromptInstance promptsInstance = new PromptInstance();
-		promptsInstance.setKey("ReferenceNum");
-		List<String> valueList = new ArrayList<String>();
-		valueList.add("019010125320");
-		promptsInstance.setValue(valueList);
-		reportPrompt.setPrompt(promptsInstance);
-		promptsList.add(reportPrompt);
+        Report report = new Report();
+        report.setId(1L);
 
-		ReportInstanceDTO reportInstanceDTO = new ReportInstanceDTO();
-		reportInstanceDTO
-				.setCreationDate(Date.from(LocalDate.of(1970, 1, 1).atStartOfDay().atZone(ZoneOffset.UTC).toInstant()));
-		reportInstanceDTO.setId(1L);
-		reportInstanceDTO.setModuleId(1L);
-		reportInstanceDTO.setPromptsList(promptsList);
-		reportInstanceDTO.setReportId(1L);
-
-		ReportContext reportContext = new ReportContext();
-		reportContext.setCountry(CountryType.UAE);
-		reportContext.setExecutionId(1L);
-		reportContext.setLinkInstanceId(1L);
-		reportContext.setLinkReference("Link Reference");
-		reportContext.setLinkedReport(true);
-		reportContext.setModuleId(1L);
-		reportContext.setReportId(1L);
-		reportContext.setReportInstance(reportInstanceDTO);
-		reportContext.setReportName("Report Name");
-		reportContext.setRoleId(1L);
-		reportContext.setRoleName("Role Name");
-		reportContext.setUserId(1L);
-		reportContext.setUserName("janedoe");
-
-		Report report = new Report();
-		report.setId(1L);
-
-		Components mockComponents = new Components();
+        Components mockComponents = new Components();
 		mockComponents.setId(1L);
 		mockComponents.setActive("Y");
 		mockComponents.setComponentKey("uaefts");
@@ -193,25 +161,31 @@ public class SnappReportServicTest {
 		mockComponents.setId(1L);
 		mockComponents.setReport(report);
 
-		ComponentDetails mockComponentDetails = new ComponentDetails();
-		mockComponentDetails.setComponents(mockComponents);
-		mockComponentDetails.setId(1L);
-		mockComponentDetails.setQuery("Select * from conf_rpt_Comp");
-		mockComponentDetails.setQueryKey("uaefts-ccn");
-		List<ComponentDetails> mockComponentDetailsList = new ArrayList<ComponentDetails>();
-		mockComponentDetailsList.add(mockComponentDetails);
+        ReportOutput reportOutput = new ReportDefaultOutput();
 
-		mockComponents.setComponentDetailsList(mockComponentDetailsList);
-		List<Components> mockComponentsList = new ArrayList<Components>();
-		mockComponentsList.add(mockComponents);
+        List<Components> componentList = new ArrayList<>();
+        componentList.add(mockComponents);
 
-		when(componentsDAO.findAllByreportId(anyLong())).thenReturn(mockComponentsList);
-		when(reportConfigurationService.fetchReportByName(Mockito.<String>any())).thenReturn(report);
-		when(queryExecutorService.executeQuery(any(ReportComponentDetailDTO.class),
-				any(ReportComponentDetailContext.class))).thenReturn(snappReportOutputList);
-		ReportExecuteResponseData response = SnappReportServiceImpl.processReport(snappDetailedReportInput,
-				reportContext);
-		verify(componentsDAO).findAllByreportId(anyLong());
-		verify(reportConfigurationService).fetchReportByName(Mockito.<String>any());
-	}
+       // List<? extends ReportOutput> outputList = new ArrayList<>();
+       // outputList.addAll( reportOutput);
+
+        List<ReportDefaultOutput> snappReportOutputList = new ArrayList<>();
+        snappReportOutputList.add((ReportDefaultOutput) reportOutput);
+
+        when(reportConfigurationService.fetchReportByName("TestReport")).thenReturn(report);
+        when(componentsDAO.findAllByreportId(1L)).thenReturn(componentList);
+       // when(snappReportConnector.processReportComponent(reportInput, reportContext)).thenReturn(outputList);
+        when(reportOutputExecutor.populateRowData(snappReportOutputList, report)).thenReturn(new ArrayList<>());
+        when(reportOutputExecutor.populateColumnDef(report)).thenReturn(new ArrayList<>());
+
+        ReportExecuteResponseData responseData = SnappReportServiceImpl.processReport(reportInput, reportContext);
+
+        assertNotNull(responseData);
+             
+       /* verify(reportConfigurationService, times(1)).fetchReportByName("TestReport");
+        verify(componentsDAO, times(1)).findAllByreportId(1L);
+        verify(snappReportConnector, times(1)).processReportComponent(reportInput, reportContext);
+        verify(reportOutputExecutor, times(1)).populateRowData(snappReportOutputList, report);
+        verify(reportOutputExecutor, times(1)).populateColumnDef(report);*/
+    }
 }
